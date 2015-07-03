97eb0874dfcb200fc7e08083357614f1e52ff117
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-03 14:54:48 +0200

    Close #1 PR: Add multiple file support using *.jpg.

--- cli.js
+++ cli.js
@@ -21,13 +21,23 @@ function init(p) {
 	process.exit(p ? 0 : 1);
 }
 
+function initArray(filename, p) {
+	console.log((p ? logSymbols.success: logSymbols.error) + ' : ' + filename);
+}
+
 if (process.stdin.isTTY) {
 	if (cli.input.length === 0) {
 		console.error('Specify a filename');
 		process.exit(2);
 	}
 
-	init(isProgressive.fileSync(cli.input[0]));
+	if (cli.input.length == 1) {
+		init(isProgressive.fileSync(cli.input[0]));
+	} else {
+		cli.input.forEach(function(element, index, array){
+			initArray(cli.input[index], isProgressive.fileSync(cli.input[index]));
+		});
+	}
 } else {
 	process.stdin.pipe(isProgressive.stream(init));
 }
